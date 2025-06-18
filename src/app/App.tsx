8035826674c97dff05799a62ac2c9e4b1edb77b4
choihooo <hochoi8621@naver.com>
2025-06-18 23:21:28 +0900

    chore: alias 설정

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className="text-9xl">
+        dasdasdas
+      </p>
+    </>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import path from "path";
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+   resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@src/components": path.resolve(__dirname, "src/components"),
+      "@src/pages": path.resolve(__dirname, "src/pages"),
+      "@src/layouts": path.resolve(__dirname, "src/layouts"),
+      "@src/utils": path.resolve(__dirname, "src/utils"),
+      "@src/hooks": path.resolve(__dirname, "src/hooks"),
+      "@src/types": path.resolve(__dirname, "src/types"),
+      "@src/styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
 })
